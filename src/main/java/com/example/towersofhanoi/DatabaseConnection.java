3a20bb65263c8e6ebff97c09f6a5110831f192c2
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-05-29 20:03:09 +0300

    connect() method implemented

--- src/main/java/com/example/towersofhanoi/DatabaseConnection.java
+++ src/main/java/com/example/towersofhanoi/DatabaseConnection.java
@@ -3,6 +3,7 @@ package com.example.towersofhanoi;
 import java.sql.*;
 public class DatabaseConnection {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String JDBC_URL = "jdbc:mysql://localhost:3306/towers-of-Hanoi";
     public void loadDriver()
     {
         try {
@@ -13,6 +14,16 @@ public class DatabaseConnection {
             return;
         }
     }
+    public void connect()
+    {
+        try {
+            Connection con = DriverManager.getConnection(JDBC_URL,"root", "");
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
 
     }
