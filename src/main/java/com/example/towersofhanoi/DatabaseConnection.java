e257050cc903069fb1e17a7306d6a3455ae12daf
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-05-29 19:58:02 +0300

    loadDriver() method implemented

--- src/main/java/com/example/towersofhanoi/DatabaseConnection.java
+++ src/main/java/com/example/towersofhanoi/DatabaseConnection.java
@@ -3,4 +3,17 @@ package com.example.towersofhanoi;
 import java.sql.*;
 public class DatabaseConnection {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public void loadDriver()
+    {
+        try {
+            Class.forName(JDBC_DRIVER);
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println(" Error. Couldn't load driver!\n" + e);
+            return;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
 }
