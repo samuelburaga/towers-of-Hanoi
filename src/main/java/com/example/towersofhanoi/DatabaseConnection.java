3fc2f005a4b8188d446263800b651aedd7c740cb
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-05-29 20:17:51 +0300

    Database connection methods implemented

--- src/main/java/com/example/towersofhanoi/DatabaseConnection.java
+++ src/main/java/com/example/towersofhanoi/DatabaseConnection.java
@@ -1,9 +1,24 @@
 package com.example.towersofhanoi;
 
+import javafx.scene.chart.PieChart;
+
 import java.sql.*;
 public class DatabaseConnection {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String JDBC_URL = "jdbc:mysql://localhost:3306/towers-of-Hanoi";
+    static final String hostname = "localhost", port = "3306", database = "towers-of-hanoi";
+    static final String JDBC_URL = "jdbc:mysql://" + hostname + "/" + port + "/" + database;
+    protected Connection connection;
+    protected Statement statement;
+
+    public DatabaseConnection(Connection connection) {
+        this.connection = connection;
+    }
+    public Connection getConnection() {
+        return this.connection;
+    }
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
     public void loadDriver()
     {
         try {
@@ -17,9 +32,26 @@ public class DatabaseConnection {
     public void connect()
     {
         try {
-            Connection con = DriverManager.getConnection(JDBC_URL,"root", "");
+            connection = DriverManager.getConnection(JDBC_URL,"root", "");
         }
         catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void Statement()
+    {
+        try {
+            this.statement = connection.createStatement();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public void executeQuery(final String query) {
+        try {
+            ResultSet resultSet = statement.executeQuery(query);
+        }
+        catch (SQLException e){
             e.printStackTrace();
         }
     }
