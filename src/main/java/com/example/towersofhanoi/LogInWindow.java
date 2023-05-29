f71a059288bc96515be7ba2b49256aeb50abf682
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-05-29 17:33:22 +0300

    Code structure for LogInWindow.java

--- src/main/java/com/example/towersofhanoi/LogInWindow.java
+++ src/main/java/com/example/towersofhanoi/LogInWindow.java
@@ -1,7 +1,11 @@
 package com.example.towersofhanoi;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LogInWindow extends Application {
 
@@ -10,7 +14,11 @@ public class LogInWindow extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage logInStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/log_in.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        logInStage.setTitle("Log In");
+        logInStage.setScene(scene);
+        logInStage.show();
     }
 }
