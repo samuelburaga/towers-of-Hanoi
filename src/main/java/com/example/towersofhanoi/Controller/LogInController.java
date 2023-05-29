ce0e005faba4a64d9d5322d1bfeb7b7f398647db
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-05-29 17:37:00 +0300

    cancelButton closing event

--- src/main/java/com/example/towersofhanoi/Controller/LogInController.java
+++ src/main/java/com/example/towersofhanoi/Controller/LogInController.java
@@ -1,12 +1,18 @@
 package com.example.towersofhanoi.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LogInController {
     @FXML
     private TextField username, password;
     @FXML
     private Button logInButton, cancelButton;
+    public void cancelButtonOnAction(ActionEvent e) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
 }
