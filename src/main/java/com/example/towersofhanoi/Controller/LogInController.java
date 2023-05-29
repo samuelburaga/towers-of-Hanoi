d0f1c0ade4073f346bed1a2e8b075b29ca304010
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-05-29 17:28:57 +0300

    Log In: Controller & FXML connection base code

--- src/main/java/com/example/towersofhanoi/Controller/LogInController.java
+++ src/main/java/com/example/towersofhanoi/Controller/LogInController.java
@@ -1,4 +1,12 @@
 package com.example.towersofhanoi.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LogInController {
+    @FXML
+    private TextField username, password;
+    @FXML
+    private Button logInButton, cancelButton;
 }
