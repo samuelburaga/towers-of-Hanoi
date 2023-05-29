dd3418cd2d07684b0969cffa92ca10a8d975b491
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-05-29 17:14:52 +0300

    Log In window test

--- src/main/java/com/example/towersofhanoi/HelloApplication.java
+++ src/main/java/com/example/towersofhanoi/HelloApplication.java
@@ -10,10 +10,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/hello-view.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setMaximized(true); // Set the window to be maximized
+        // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/log_in.fxml"));
+        // Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        // Scene scene = new Scene(fxmlLoader.load());
+        // stage.setMaximized(true); // Set the window to be maximized
         stage.setTitle("Towers of Hanoi");
         stage.setScene(scene);
         stage.show();
