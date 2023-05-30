bd030ca36f374734dd4513e552fa100c2ea7eb73
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-05-30 14:45:51 +0300

    Window Full Screen mode fixed

--- src/main/java/com/example/towersofhanoi/Menu.java
+++ src/main/java/com/example/towersofhanoi/Menu.java
@@ -2,7 +2,9 @@ package com.example.towersofhanoi;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -17,9 +19,9 @@ public class Menu extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View/Menu.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load(), 1000, 1080);
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         primaryStage.setMaximized(true);
+        primaryStage.setFullScreen(true);
         primaryStage.setTitle("Menu");
         primaryStage.setScene(scene);
         primaryStage.show();
