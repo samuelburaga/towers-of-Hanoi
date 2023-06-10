9dda9d7ab72862fe6ff9cd6716a84f872ed5ff42
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-06-10 19:08:28 +0300

    Quit button works

--- src/main/java/com/example/towersofhanoi/Controller/MenuController.java
+++ src/main/java/com/example/towersofhanoi/Controller/MenuController.java
@@ -1,4 +1,15 @@
 package com.example.towersofhanoi.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class MenuController {
+    @FXML
+    private Button quitButton;
+    public void quitButtonOnAction(ActionEvent e) {
+        Stage stage = (Stage) quitButton.getScene().getWindow();
+        stage.close();
+    }
 }
