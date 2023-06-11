273d7774dd592a7bd935b288517dad1ec793a1c9
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-06-11 11:17:04 +0300

    Thread code works

--- src/main/java/com/example/towersofhanoi/TutorialThread.java
+++ src/main/java/com/example/towersofhanoi/TutorialThread.java
@@ -1,8 +1,21 @@
 package com.example.towersofhanoi;
 
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class TutorialThread extends Thread{
     public TutorialThread() {
     }
     public void run() {
+        Platform.runLater(() -> {
+            Tutorial tutorial = new Tutorial();
+            try {
+                tutorial.start(new Stage());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
