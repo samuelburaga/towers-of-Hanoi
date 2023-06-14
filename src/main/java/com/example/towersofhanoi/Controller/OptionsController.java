d62e34f2f876b5e723dd633920e43b2206d4e5ce
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-06-14 16:25:22 +0300

    last Option commit

--- src/main/java/com/example/towersofhanoi/Controller/OptionsController.java
+++ src/main/java/com/example/towersofhanoi/Controller/OptionsController.java
@@ -1,34 +1,50 @@
 package com.example.towersofhanoi.Controller;
 
 import com.example.towersofhanoi.Game;
+import com.example.towersofhanoi.Menu;
 import com.example.towersofhanoi.Tutorial;
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class OptionsController {
     @FXML
     private Label option1, option2, option3, option4, option5, option6, option7, option8, option9, option10;
+    @FXML
+    private Label previousClickedLabel;
     public void optionOnMouseClicked(MouseEvent e) {
         Label clickedLabel = (Label) e.getSource();
-        // System.out.println("Clicked label: " + clickedLabel.getText());
+        String clickedColor = "-fx-background-color: #0FB4BB;"; // Set the desired color
+        String defaultColor = "-fx-background-color: #333E41;"; // Set the default color
         Game.disks = Byte.parseByte(clickedLabel.getText());
+        if (previousClickedLabel != null) {
+            previousClickedLabel.setStyle(defaultColor); // Revert the color of the previously clicked label
+        }
+        clickedLabel.setStyle(clickedColor); // Set the color for the newly clicked label
+        previousClickedLabel = clickedLabel; // Update the previously clicked label
         System.out.println(Game.disks);
     }
     public void backButtonOnAction(ActionEvent e) throws IOException {
         Node node = (Node) e.getSource();
         Stage thisStage = (Stage) node.getScene().getWindow();
-        thisStage.hide();
+        Menu menu = new Menu();
+        menu.start(new Stage());
     }
     public void playNowButtonOnAction(ActionEvent e) throws IOException {
         Node node = (Node) e.getSource();
         Stage thisStage = (Stage) node.getScene().getWindow();
-        thisStage.hide();
+
     }
 }
--- src/main/java/com/example/towersofhanoi/Options.java
+++ src/main/java/com/example/towersofhanoi/Options.java
@@ -12,14 +12,13 @@ public class Options extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("View/Options.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         primaryStage.setMaximized(true);
         primaryStage.setFullScreen(true);
-        primaryStage.setTitle("Menu");
+        primaryStage.setTitle("Options");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
