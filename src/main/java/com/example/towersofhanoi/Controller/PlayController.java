dd3e4500c88e521a38c25b10990d978a526f1498
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-06-14 09:24:46 +0300

    Play window works

--- src/main/java/com/example/towersofhanoi/Controller/PlayController.java
+++ src/main/java/com/example/towersofhanoi/Controller/PlayController.java
@@ -1,4 +1,49 @@
 package com.example.towersofhanoi.Controller;
 
+import com.example.towersofhanoi.Game;
+import com.example.towersofhanoi.Tutorial;
+import javafx.animation.PauseTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
 public class PlayController {
+    @FXML
+    private Pane rodA, rodB, rodC;
+    @FXML
+    private Button AToBButton, AToCButton, BToAButton, BToCButton, CToAButton, CToBButton;
+    private byte numberOfDisks;
+    private String move;
+    public void setNumberOfDisks(byte numberOfDisks) {
+        this.numberOfDisks = numberOfDisks;
+        createDisks();
+    }
+    private void createDisks() {
+        rodA.getChildren().clear(); // Clear any existing disks
+        double diskWidth = 198.0; // Adjust as needed
+        double diskHeight = 60.0; // Adjust as needed
+        double initialX = 0, initialY = rodA.getPrefHeight() - diskHeight; // Adjust as needed
+        double arcWidth = 30.0;
+        double arcHeight = 30.0;
+        for (byte i = 0; i < numberOfDisks; i++) {
+            Rectangle disk = new Rectangle(diskWidth - (i * 12), diskHeight);
+            disk.setX(initialX + (i * 6));
+            disk.setLayoutY(initialY - (i * diskHeight));
+            disk.setArcHeight(arcHeight);
+            disk.setArcWidth(arcWidth);
+            disk.setFill(Color.valueOf("#0FB4BB")); // Adjust as needed
+            disk.setStroke(Color.WHITE); // Adjust as needed
+            rodA.getChildren().add(disk);
+        }
+    }
 }
--- src/main/java/com/example/towersofhanoi/Play.java
+++ src/main/java/com/example/towersofhanoi/Play.java
@@ -1,7 +1,13 @@
 package com.example.towersofhanoi;
 
+import com.example.towersofhanoi.Controller.PlayController;
+import com.example.towersofhanoi.Controller.TutorialController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Play extends Application {
 
@@ -10,7 +16,17 @@ public class Play extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("View/Play.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
+        // Pass the number of disks to the controller
+        PlayController controller = fxmlLoader.getController();
+        byte numberOfDisks = Game.disks; // Replace with the actual number of disks
+        controller.setNumberOfDisks(numberOfDisks);
+
+        primaryStage.setTitle("Play");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
