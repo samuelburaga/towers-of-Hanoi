099c1d413c0e6089d2029f7df89fb57ca16bc9ec
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-06-15 18:53:14 +0300

    statistics queries file

--- src/database/usersQueries.sql
+++ src/database/usersQueries.sql
@@ -1,11 +1,11 @@
-INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Samuel", "Buraga", "sami", "1234", NOW());
-INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("David", "Buraga", "davidburaga", "12345", NOW());
-INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Adrian", "Oprisor", "adioprisor", "123456", NOW());
-INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Ion", "Popescu", "ion", "password_ion", NOW());
-INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Vasile", "Afloarei", "vasile", "password_vasile", NOW());
-INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Andreea", "Amariei", "andreea", "password_andreea", NOW());
-INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Eliza", "Popescu", "eliza", "password_eliza", NOW());
-INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Tudor", "Miron", "tudor", "password_tudor", NOW());
-INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Darian", "Maria", "darian", "password_darian", NOW());
-INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Emi", "Munteanu", "emi", "password_emi", NOW());
+-- INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Samuel", "Buraga", "sami", "1234", NOW());
+-- INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("David", "Buraga", "davidburaga", "12345", NOW());
+-- INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Adrian", "Oprisor", "adioprisor", "123456", NOW());
+-- INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Ion", "Popescu", "ion", "password_ion", NOW());
+-- INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Vasile", "Afloarei", "vasile", "password_vasile", NOW());
+-- INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Andreea", "Amariei", "andreea", "password_andreea", NOW());
+-- INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Eliza", "Popescu", "eliza", "password_eliza", NOW());
+-- INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Tudor", "Miron", "tudor", "password_tudor", NOW());
+-- INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Darian", "Maria", "darian", "password_darian", NOW());
+-- INSERT INTO users (first_name, last_name, username, password, created_at) VALUES ("Emi", "Munteanu", "emi", "password_emi", NOW());
 SELECT * FROM users
