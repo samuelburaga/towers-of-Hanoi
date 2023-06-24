0c9c8d3874795712e28331044dd9cf093aa239a0
samuelburaga <116203173+samuelburaga@users.noreply.github.com>
2023-06-24 12:12:48 +0300

    MongoDBConnection class

---
+++ src/main/java/com/example/towersofhanoi/MongoDBConnection.java
@@ -0,0 +1,12 @@
+package com.example.towersofhanoi;
+
+public class MongoDBConnection implements Database {
+    @Override
+    public void connect() {
+
+    }
+    @Override
+    public void disconnect() {
+
+    }
+}
